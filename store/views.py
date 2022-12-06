b1868f61b6348fff6c1196accdd30f5280c2556c
Jakub Żmudziński <kubaz8645@gmail.com>
2022-12-06 23:05:26 +0100

    permissions and filters cutomization

--- store/views.py
+++ store/views.py
@@ -8,11 +8,17 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Product, ProductImage, Promotion, Collection
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ProductSerializer
+from .permissions import IsAdminOrReadOnly
+from .filters import ProductFilter
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.prefetch_related('images').all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAdminOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProductFilter
+    search_fields = ['title']
+    ordering_fields = ['price', 'inventory']
 
     def get_serializer_context(self):
         return {'request': self.request}
